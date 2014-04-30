6d3a1b040010d385c123f45e2047339f8057a8c3
Fred Willmore <fredwillmore@gmail.com>
2014-04-30 15:16:26 -0700

    add contact form, responsive layout tweaks

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+  before_action :set_parent_categories
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+      flash.now[:notice] = 'Thank you for your message!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
+
+  private
+  def set_parent_categories
+    @parent_categories = ParentCategory.all
+  end
+end
